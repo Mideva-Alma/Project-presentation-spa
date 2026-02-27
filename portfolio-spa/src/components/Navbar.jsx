341c9a4ed1bea6cd81801239c19e37d77fc0e584
Mideva-Alma <aileenmideva@email.com>
2026-02-27 03:42:12 +0300

    navbar added

---
+++ portfolio-spa/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+function Navbar() {
+	return (
+		<nav style={{ padding: '10px 0', borderBottom: '1px solid #ccc', marginBottom: '20px' }}>
+			<h2>Portfolio Platform</h2>
+		</nav>
+	);
+}
+
+export default Navbar;
